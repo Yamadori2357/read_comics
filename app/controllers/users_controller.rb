8662475d34a0ffbff9ce2f8b64831fca5bbb9503
Yamadori2357 <abcdabcd2357@gmail.com>
2020-09-03 02:20:17 +0000

    serch_users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:profile_edit, :profile_update, :password_edit, :password_update]
+  before_action :logged_in_user, only: [:profile_edit, :profile_update, :password_edit, :password_update, :destroy]
   before_action :correct_user, only: [:profile_edit, :profile_update, :password_edit, :password_update]
+  before_action :admin_user,     only: :destroy
   
+
 
   def show
     @user = User.find(params[:id])
@@ -58,13 +60,17 @@ class UsersController < ApplicationController
   
   def search_users
     if params[:name].present?
-      @users = User.where('name LIKE ?', "%#{params[:name]}%")
+      @users = User.where('name LIKE ?', "%#{params[:name]}%").paginate(page: params[:page])
     else
-      @users = User.none
+      @users = User.paginate(page: params[:page])
     end
   end
 
-  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
  
   private
@@ -88,6 +94,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうか確認
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
     
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_063033) do
+ActiveRecord::Schema.define(version: 2020_09_03_020122) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,10 +39,12 @@ ActiveRecord::Schema.define(version: 2020_09_01_063033) do
     t.string "self_instoduction"
     t.string "profile_image"
     t.string "header_image"
+    t.date "birth_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
